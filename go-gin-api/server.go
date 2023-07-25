5ed702da4a2bc8d108f7e4939384b7514ee1d57d
KuroNgo <hoaiphong01012002@gmail.com>
2023-07-25 22:56:44 +0700

    update gin-api-no-sql

--- go-gin-api/server.go
+++ go-gin-api/server.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"kuro.com/pragnaticreviews/golang-gin-poc/controller"
+	"kuro.com/pragnaticreviews/golang-gin-poc/middleware"
 	"kuro.com/pragnaticreviews/golang-gin-poc/service"
 )
 
@@ -11,14 +15,31 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+	setupLogOutput()
+	// tạo đối tượng *gin.Engine đã được cấu hình sẵn
+	// server := gin.Default()
 	// server.GET("/test", func(ctx *gin.Context) {
 	// 	ctx.JSON(200, gin.H{
 	// 		"message": "OK!!",
 	// 	})
 	// })
+	// TODO: middleware
+	// tạo mới một instance của framework Gin
+	server := gin.New()
 
+	// tự động xxuwr lý các panic lỗi runtime trong ứng dụng và
+	// trả về một HTTP 500 Internal Server Error
+	server.Use(gin.Recovery(), middleware.Logger())
+
+	// để ghi log các yêu cầu HTTP và các thông tin liên quan
+	// server.Use(gin.Logger())
+	// Định nghĩa các route và xử lý request tại đây
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
 	})
