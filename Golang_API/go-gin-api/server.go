7e0e52d1a455ce253c079209dba9dfe264113157
KuroNgo <hoaiphong01012002@gmail.com>
2023-07-26 22:23:51 +0700

    update gin-api-no-sql

--- Golang_API/go-gin-api/server.go
+++ Golang_API/go-gin-api/server.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"io"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 	"kuro.com/pragnaticreviews/golang-gin-poc/controller"
 	"kuro.com/pragnaticreviews/golang-gin-poc/middleware"
 	"kuro.com/pragnaticreviews/golang-gin-poc/service"
@@ -32,21 +34,34 @@ func main() {
 	// TODO: middleware
 	// tạo mới một instance của framework Gin
 	server := gin.New()
+	server.Static("/css", "/template/css")
 
+	server.LoadHTMLGlob("templates/html/*.html")
+	apiRoutes := server.Group("/api")
+	{
+		// để ghi log các yêu cầu HTTP và các thông tin liên quan
+		// server.Use(gin.Logger())
+		// Định nghĩa các route và xử lý request tại đây
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(200, videoController.FindAll())
+		})
+
+		apiRoutes.POST("/postvideo", func(ctx *gin.Context) {
+			err := videoController.Save(ctx)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is Valid!"})
+			}
+		})
+	}
 	// tự động xxuwr lý các panic lỗi runtime trong ứng dụng và
 	// trả về một HTTP 500 Internal Server Error
-	server.Use(gin.Recovery(), middleware.Logger())
-
-	// để ghi log các yêu cầu HTTP và các thông tin liên quan
-	// server.Use(gin.Logger())
-	// Định nghĩa các route và xử lý request tại đây
-	server.GET("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.FindAll())
-	})
-
-	server.POST("/postvideo", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.Save(ctx))
-	})
-
+	server.Use(gin.Recovery(), middleware.Logger(),
+		middleware.Auth(), gindump.Dump())
+	viewRoutes := server.Group("/view")
+	{
+		viewRoutes.GET("/videos", videoController.ShowAll)
+	}
 	server.Run(":8080")
 }
